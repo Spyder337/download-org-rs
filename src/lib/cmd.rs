3ff1744958116e13f23fb843bee2aadd48828597
Owsley Wood <owsley.wood@gmail.com>
2024-09-08 02:34:42 -0700

    Added cleaning command for deleting items.

--- src/lib/cmd.rs
+++ src/lib/cmd.rs
@@ -2,23 +2,22 @@ use std::{path::PathBuf, str::FromStr};
 
 use clap::{command, Parser, Subcommand, ValueEnum};
 
-use crate::lib::organizer::os_str_to_string;
+use super::organizer::{self, os_str_to_string, Organizer};
 
-use super::organizer::{self, Organizer};
-
-pub(crate) fn execute(organizer: &mut Organizer) -> (){
+/// Runs clap with an [`Organizer`] as a parameter that contains the settings.
+pub(crate) fn execute(organizer: &mut Organizer) {
     let cli = Cli::parse();
 
     match &cli.command {
         Some(Commands::Sort { verbose }) => match verbose {
             Some(v) => {
-                organizer::sort_downloads(&organizer, *v);
+                organizer::sort_downloads(organizer, *v);
             }
-            None => organizer::sort_downloads(&organizer, false),
+            None => organizer::sort_downloads(organizer, false),
         },
         Some(Commands::Assign { path }) => {
             let path_buf: PathBuf = PathBuf::from(&path);
-            organizer.sorting_path = PathBuf::from(path_buf);
+            organizer.sorting_path = path_buf;
             organizer.save_file()
         }
         Some(Commands::Display { mode }) => match mode {
@@ -37,15 +36,20 @@ pub(crate) fn execute(organizer: &mut Organizer) -> (){
             dest_path.push(&organizer.sorting_path);
             dest_path.push(dest);
             let filter: String = String::from(extension);
-            organizer.rules.insert(filter, os_str_to_string(dest_path.as_os_str()));
+            organizer
+                .rules
+                .insert(filter, os_str_to_string(dest_path.as_os_str()));
             organizer.save_file();
-        }
-        Some(Commands::Save) => organizer.save_file(),
+        },
+        Some(Commands::Clean { mode, in_str }) => {
+            let removed_cnt: usize = organizer.run_clean(mode, in_str);
+            println!("Deleted {} items.", removed_cnt);
+        },
         None => println!("Usage: download-org.exe help"),
     }
 }
 
-
+/// Container for the main command line interface.
 #[derive(Parser)]
 #[command(version, about,  long_about = None)]
 struct Cli {
@@ -72,12 +76,37 @@ impl ToString for DisplayMode {
     }
 }
 
+/// Reprsents different modes of the cleaning method.
+#[derive(ValueEnum, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub(crate) enum CleaningMode {
+    /// Represents removing files by extension.
+    Extension,
+    /// Represents removing files in a sub-directory.
+    Directory,
+    /// Represents removing all files in the sort directory..
+    RootDir,
+}
+
+impl ToString for CleaningMode {
+    fn to_string(&self) -> String {
+        match &self {
+            CleaningMode::Extension => String::from_str("Extension").unwrap(),
+            CleaningMode::Directory => String::from_str("Directory").unwrap(),
+            CleaningMode::RootDir => String::from_str("Root Directory").unwrap(),
+        }
+    }
+}
+
+//  Implement a cleaning command that can remove all items of a type, a directory, or
+//  the entire root directory.
+
+/// Commands that can be executed via Cli::parse().
 #[derive(Subcommand)]
 enum Commands {
     /// Sorts the files in the sorting directory.
     Sort {
         /// Whether to output verbose messages.
-        #[arg(short = 'v',)]
+        #[arg(short = 'v')]
         verbose: Option<bool>,
     },
     /// Assign a new path as the sorting directory.
@@ -101,6 +130,13 @@ enum Commands {
         #[arg(short, help = "The destination relative to the sorting directory.")]
         dest: String,
     },
-    /// Save the current settings to a file.
-    Save,
+    Clean {
+        #[arg(short, help = "Determines which files are deleted.", 
+            default_value_t = CleaningMode::Extension)]
+        mode: CleaningMode,
+        #[arg(short,
+            default_value_t=String::from_str(".exe").unwrap())]
+        /// Required input for [`CleaningMode::Extension`] and [`CleaningMode::Directory`].
+        in_str: String
+    }
 }
--- src/lib/organizer.rs
+++ src/lib/organizer.rs
@@ -1,32 +1,46 @@
-use std::{collections::BTreeMap, ffi::OsStr, fs::{self}, path::{Path, PathBuf}, str::FromStr};
 use glob::glob;
 use serde::{Deserialize, Serialize};
+use std::{
+    collections::BTreeMap,
+    ffi::OsStr,
+    fs::{self},
+    path::{Path, PathBuf},
+    str::{FromStr},
+};
+
+use super::cmd::CleaningMode;
 
 const FILE_NAME: &str = "dorg_config.json";
 
+/// Represents the config for sorting files.
 #[derive(Serialize, Deserialize)]
-pub(crate) struct Organizer{
+pub(crate) struct Organizer {
     /// A rule is a (File Extension, Relative Path) Key Value pair.
     pub rules: BTreeMap<String, String>,
-    /// The destination folder that will be sorted and 
+    /// The destination folder that will be sorted and
     /// combined with a Value from the [`Organizer::rules`].
     pub sorting_path: PathBuf,
 }
 
 impl Default for Organizer {
     fn default() -> Self {
-        Self { rules: Organizer::get_default_rules(), sorting_path: get_downloads_folder() }
-    } 
+        Self {
+            rules: Organizer::get_default_rules(),
+            sorting_path: get_downloads_folder(),
+        }
+    }
 }
 
-impl Organizer{
-
+impl Organizer {
     /// Creates a new [`Organizer`].
     pub fn new(rules: BTreeMap<String, String>) -> Self {
-        Self { rules, sorting_path: get_downloads_folder() }
+        Self {
+            rules,
+            sorting_path: get_downloads_folder(),
+        }
     }
 
-    pub fn save_file(&self) -> () {
+    pub fn save_file(&self) {
         let path = get_settings_folder();
         //  Handle path parsing and errors
         if !Path::exists(&path) {
@@ -35,9 +49,9 @@ impl Organizer{
                 panic!("{:?}", res.unwrap())
             }
         }
-        let mut full_path = PathBuf::from(path);
+        let mut full_path = path;
         full_path.push(FILE_NAME);
-        
+
         let deserialized = serde_json::to_string_pretty(&self);
 
         //  Verify successful deserialization.
@@ -48,7 +62,7 @@ impl Organizer{
                     Ok(_) => println!("Successfully saved settings to {:?}", full_path),
                     Err(e) => panic!("{:?}", e),
                 }
-            },
+            }
             //  Deserialization error.
             Err(e) => panic!("{:?}", e),
         }
@@ -62,12 +76,12 @@ impl Organizer{
         match read_file_res {
             Ok(data) => {
                 let org: Organizer = serde_json::from_str(data.as_str()).unwrap();
-                return org;
-            },
+                org
+            }
             Err(e) => panic!("{:?}", e),
         }
     }
-    
+
     pub fn get_default_rules() -> BTreeMap<String, String> {
         let mut rules: BTreeMap<String, String> = BTreeMap::new();
         let mut exe_path = PathBuf::new();
@@ -75,17 +89,20 @@ impl Organizer{
         let mut meta_path = PathBuf::new();
         meta_path.push("MetaInfo");
         rules.insert("exe".to_string(), os_str_to_string(exe_path.as_os_str()));
-        rules.insert("torrent".to_string(), os_str_to_string(meta_path.as_os_str()));
+        rules.insert(
+            "torrent".to_string(),
+            os_str_to_string(meta_path.as_os_str()),
+        );
         rules
     }
 
-    pub fn display_rules(&self) -> (){
+    pub fn display_rules(&self) {
         for r in &self.rules {
             println!("{} : {}", r.0, r.1)
         }
     }
 
-    pub fn display_directory(&self) -> () {
+    pub fn display_directory(&self) {
         let items = self.get_downloaded_items();
         let dirs = self.get_downloads_sub_dirs();
         println!("Directories: ");
@@ -97,12 +114,12 @@ impl Organizer{
             println!("{}", i);
         }
     }
-    
+
     pub(crate) fn get_downloaded_items(&self) -> Vec<String> {
         let sort_dir = PathBuf::from_str(self.sorting_path.to_str().unwrap()).unwrap();
         let pattern = format!("{}/*.*", os_str_to_string(sort_dir.as_os_str()));
         let mut items: Vec<String> = Vec::new();
-        for entry in glob(&pattern).expect("Failed to read glob pattern"){
+        for entry in glob(&pattern).expect("Failed to read glob pattern") {
             match entry {
                 Ok(path) => items.push(os_str_to_string(path.as_os_str())),
                 Err(e) => println!("{:?}", e),
@@ -110,10 +127,10 @@ impl Organizer{
         }
         items
     }
-    
+
     fn get_downloads_sub_dirs(&self) -> Vec<String> {
         let down_dir = PathBuf::from(&self.sorting_path);
-        let pattern = format!("{}/**/",os_str_to_string(down_dir.as_os_str()));
+        let pattern = format!("{}/**/", os_str_to_string(down_dir.as_os_str()));
         let mut dirs: Vec<String> = Vec::new();
         for entry in glob(&pattern).expect("Failed to read glob pattern.") {
             match entry {
@@ -123,10 +140,49 @@ impl Organizer{
         }
         dirs
     }
+
+    pub fn run_clean(&self, mode: &CleaningMode, in_str: &String) -> usize {
+        let remove_cnt: usize;
+        
+        match &mode {
+            CleaningMode::Extension => {
+                let base_path = PathBuf::from(&self.sorting_path);
+                let pattern = format!("{}/**/*.{}", os_str_to_string(base_path.as_os_str()), in_str);
+                remove_cnt = Self::glob_remove(pattern);
+            },
+            CleaningMode::Directory => {
+                let mut base_path = PathBuf::from(&self.sorting_path);
+                base_path.push(in_str);
+                let pattern = format!("{}/**/*.*", os_str_to_string(base_path.as_os_str()));
+                remove_cnt = Self::glob_remove(pattern);
+            },
+            CleaningMode::RootDir => {
+                let base_path = PathBuf::from(&self.sorting_path);
+                let pattern = format!("{}/**/*.*", os_str_to_string(base_path.as_os_str()));
+                remove_cnt = Self::glob_remove(pattern);
+            },
+        }
+    
+        remove_cnt
+    }
+
+    pub(self) fn glob_remove(pattern: String) -> usize {
+        let mut remove_cnt: usize = 0;
+        for entry in glob(&pattern).expect("") {
+            match entry {
+                Ok(path) => {
+                    remove_cnt += 1;
+                    fs::remove_file(path).unwrap();
+                },
+                Err(e) => println!("Could not read the path. Error\n{:?}", e),
+            }
+        }
+        remove_cnt
+    }
 }
 
 pub(crate) fn sort_downloads(organizer: &Organizer, verbose: bool) {
-    let items: Vec<String> = organizer.get_downloaded_items();  
+    let items: Vec<String> = organizer.get_downloaded_items();
     sort_items(&items, organizer, verbose);
 }
 
@@ -145,14 +201,17 @@ fn sort_items(paths: &[String], organizer: &Organizer, verbose: bool) {
             //  Prepend the destination path root.
             new_path.push(&organizer.sorting_path);
             //  Append the relative folder path to the new path.
-            new_path.push(organizer.rules[&ext].to_string());
+            new_path.push(&organizer.rules[&ext]);
             //  Contains the new full path including file name.
             let mut new_file_path = PathBuf::new();
             new_file_path.push(&new_path);
             new_file_path.push(file_name);
-            
+
             if verbose {
-                println!("Destinattion: \"{}\"", os_str_to_string(new_path.as_os_str()))
+                println!(
+                    "Destinattion: \"{}\"",
+                    os_str_to_string(new_path.as_os_str())
+                )
             }
 
             if !fs::exists(&new_path).unwrap() {
@@ -166,7 +225,6 @@ fn sort_items(paths: &[String], organizer: &Organizer, verbose: bool) {
 pub(crate) fn os_str_to_string(s: &OsStr) -> String {
     return s.to_str().unwrap().to_string();
 }
-
 
 pub(crate) fn get_downloads_folder() -> PathBuf {
     dirs_next::download_dir().unwrap()
@@ -193,3 +251,4 @@ pub(crate) fn load_organizer() -> Organizer {
     }
     organizer
 }
+
